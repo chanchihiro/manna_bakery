58cc001c6d5ccafc006462a7733f67a89ca7f8c5
chihiro <chanchi0707@gmail.com>
2017-10-24 19:20:59 +0900

    newsのレイアウトを組んだ

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 import $ from 'jquery';
-import login from './components/login.js';
 // import character_action from './components/character_action.js';
 // こっからスタート
 document.addEventListener('DOMContentLoaded', function() {
