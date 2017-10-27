77016e893fcf3afc4ccbeef70a14c89667f58974
chihiro <chanchi0707@gmail.com>
2017-10-27 23:50:40 +0900

    googlemapの色指定

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,4 @@
 import $ from 'jquery';
-import googlemap from './components/googlemap.js';
 // import character_action from './components/character_action.js';
 // こっからスタート
 document.addEventListener('DOMContentLoaded', function() {
